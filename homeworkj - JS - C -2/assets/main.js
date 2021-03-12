530845abac555c0a34e7353c13aa2189d092ec5c
Dmytro Yakimenko <72745066+webdevDmYa@users.noreply.github.com>
2021-03-12 22:20:06 +0200

    Add files via upload

---
+++ homeworkj - JS - C -2/assets/main.js
@@ -0,0 +1,89 @@
+
+const URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/ovdp?json';
+
+
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', URL);
+
+xhr.onload = function () {
+
+	let data = JSON.parse(xhr.responseText);
+
+
+
+	/* Далее, в этой функции, вам доступна переменная data с загруженными данными */
+	let today = '2021-03-12';//
+
+	console.log(today);
+
+
+	let all = new Object();
+
+
+	for (let item of data) {
+		let repaydate = item.repaydate.split('.').reverse().join('-');
+
+		if (repaydate > today) {
+			if (item.valcode == 'USD') {
+				item.attraction = item.attraction * 28;
+
+			} else if (item.valcode == 'EUR') {
+				item.attraction = item.attraction * 32;
+			} else {
+				item.attraction = item.attraction;
+			}
+			if (all[repaydate]) {
+				all[repaydate] += item.attraction;
+			} else {
+				all[repaydate] = item.attraction;
+			}
+		}
+		if (repaydate < today) {
+			if (item.valcode == 'USD') {
+				item.attraction = item.attraction * 28;
+
+			} else if (item.valcode == 'EUR') {
+				item.attraction = item.attraction * 32;
+			} else {
+				item.attraction = item.attraction;
+			}
+			if (all[repaydate]) {
+				all[repaydate] += item.attraction;
+			} else {
+				all[repaydate] = item.attraction;
+			}
+		}
+
+	}
+	for (let key in all) {
+		all[key] = +all[key].toFixed(2);
+		if (all[key] == 0) {
+			delete all[key];
+		}
+	}
+
+	arr = Object.entries(all);
+
+	arr.sort();
+	/*	for (let j = 0; j < arr.length - 1; j++) {
+			for (i = 0; i < arr.length - j - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					let temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+		}
+	*/
+
+	console.log(arr)
+
+
+
+
+
+
+}
+xhr.send();
